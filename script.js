416968c3b4054832e5cfcb8fa554bb054bac4f04
astra-we <emiv6442@gmail.com>
2026-07-11 02:36:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const sections = document.querySelectorAll(
+".hero, .about, .rooms, .services, .footer"
+);
 
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:0.2
+});
+
+sections.forEach(section=>{
+    observer.observe(section);
+});
